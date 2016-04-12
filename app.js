80fe2a0d4a0c0d544ed3b4035d0300f5a840876f
tetsaiga86 <tetsaiga86@gmail.com>
2016-04-12 14:48:41 -0700

    final JS with 4 questions

--- app.js
+++ app.js
@@ -25,3 +25,31 @@ if (secondQuestion.toLowerCase() === 'n' || secondQuestion.toLowerCase() === 'no
 } else {
   alert ('directions are hard');
 }
+
+var thirdQuestion = prompt('Was my last job at Joongbu University?');
+console.log('thirdQuestion: ' + thirdQuestion);
+
+if (thirdQuestion.toLowerCase() === 'y' || thirdQuestion.toLowerCase() === 'yes')
+{
+  alert ('Good Job! You passed round 3!');
+} else if (thirdQuestion.toLowerCase() === 'n' || thirdQuestion.toLowerCase() === 'no'){
+  alert ('You failed. So, so much fail...');
+} else {
+  alert ('directions are hard');
+}
+
+var fourthQuestion = prompt('Will you hire me?');
+console.log('fourthQuestion: ' + fourthQuestion);
+
+if (fourthQuestion.toLowerCase() === 'y' || fourthQuestion.toLowerCase() === 'yes')
+{
+  alert ('Congratulations, you win me!!! The bestest prize of all!!!!!');
+}
+else if (fourthQuestion.toLowerCase() === 'n' || fourthQuestion.toLowerCase() === 'no')
+{
+  alert ('You will be the first to be conquered, when I take over the world!!!');
+}
+else
+{
+  alert ('directions are hard');
+}
